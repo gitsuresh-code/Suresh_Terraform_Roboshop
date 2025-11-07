c8dca2773bbcde6a2c5a2a37d47985b9c9d28586
SureshU <sureshdevops@gmail.com>
2025-11-07 22:35:53 +0530

    suresh

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -24,10 +24,10 @@ variable "sg_names" {
 }
 
 variable "zone_id" {
-    default = "Z0948150OFPSYTNVYZOY"
+    default = "Z04272802545XSOGBRNOM"
 }
 
 variable "domain_name" {
-    default = "daws86s.fun"
+    default = "sureshdevops.fun"
 }
 
