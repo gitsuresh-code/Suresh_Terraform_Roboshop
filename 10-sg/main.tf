98854f30df3b3b09dae150bf561b09e495a65829
SureshU <sureshdevops@gmail.com>
2025-11-09 20:28:57 +0530

    latest

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -10,15 +10,5 @@ module "sg" {
 
 
 
-#Frontend servers accepting traffic from frontend ALB
-resource "aws_security_group_rule" "frontend_frontend_alb" {
-  type              = "ingress"
-  security_group_id = module.sg[9].sg_id # frontend SG ID
-  source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
-  from_port         = 80
-  protocol          = "tcp"
-  to_port           = 80
-}
-
 
 
