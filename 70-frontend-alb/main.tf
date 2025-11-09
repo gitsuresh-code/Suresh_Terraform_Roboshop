450c20db681430ea11b16e2c8696a62137a22f24
SureshU <sureshdevops@gmail.com>
2025-11-09 20:47:44 +0530

    latest

--- 70-frontend-alb/main.tf
+++ 70-frontend-alb/main.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "frontend_alb" {
+resource "aws_lb" "frontend-alb" {
   name               = "${local.common_name_suffix}-frontend_alb" # roboshop-dev-frontend-alb
   internal           = true
   load_balancer_type = "application"
@@ -11,14 +11,14 @@ resource "aws_lb" "frontend_alb" {
   tags = merge (
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-frontend_alb"
+        Name = "${local.common_name_suffix}-frontend-alb"
     }
   )
 }
 
 # Backend ALB listening on port number 80
-resource "aws_lb_listener" "frontend_alb" {
-  load_balancer_arn = aws_lb.frontend_alb.arn
+resource "aws_lb_listener" "frontend-alb" {
+  load_balancer_arn = aws_lb.frontend-alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -33,15 +33,15 @@ resource "aws_lb_listener" "frontend_alb" {
   }
 }
 
-resource "aws_route53_record" "frontend_alb" {
+resource "aws_route53_record" "frontend-alb" {
   zone_id = var.zone_id
   name    = "${var.domain_name}"
   type    = "A"
 
   alias {
     # These are ALB details, not our domain details
-    name                   = aws_lb.frontend_alb.dns_name
-    zone_id                = aws_lb.frontend_alb.zone_id
+    name                   = aws_lb.frontend-alb.dns_name
+    zone_id                = aws_lb.frontend-alb.zone_id
     evaluate_target_health = true
   }
 }
